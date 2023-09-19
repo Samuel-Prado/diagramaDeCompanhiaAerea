e4d569b5f97c424363d29b48021eb08bd7247cc8
Samuel-Prado <samuel.prado@estudante.iftm.edu.br>
2023-09-19 11:20:04 -0300

    diagramVoo

---
+++ src/domain/Cidade.java
@@ -0,0 +1,60 @@
+package domain;
+
+import java.util.List;
+
+public class Cidade {
+
+    private Integer codigo;
+
+    private String nome;
+
+    private String populacao;
+
+    private List<Aeroporto> aeroportos;
+
+    public Cidade(Integer codigo, String nome, String populacao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.populacao = populacao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPopulacao() {
+        return populacao;
+    }
+
+    public void setPopulacao(String populacao) {
+        this.populacao = populacao;
+    }
+
+    public List<Aeroporto> getAeroportos() {
+        return aeroportos;
+    }
+
+    public void setAeroportos(List<Aeroporto> aeroportos) {
+        this.aeroportos = aeroportos;
+    }
+
+    @Override
+    public String toString() {
+        return "Cidade [codigo=" + codigo + ", nome=" + nome + ", populacao=" + populacao + ", aeroportos=" + aeroportos
+                + "]";
+    }
+
+    
+}
---
+++ src/domain/Passagem.java
@@ -0,0 +1,58 @@
+package domain;
+
+import java.util.List;
+
+public class Passagem {
+    
+    private Integer codigo;
+
+    private Double valor;
+
+    private Passageiro cpf;
+
+    private List<Voo> voos;
+
+    public Passagem(Integer codigo, Double valor, Passageiro cpf) {
+        this.codigo = codigo;
+        this.valor = valor;
+        this.cpf = cpf;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Passageiro getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Passageiro cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<Voo> getVoos() {
+        return voos;
+    }
+
+    public void setVoos(List<Voo> voos) {
+        this.voos = voos;
+    }
+
+    @Override
+    public String toString() {
+        return "Passagem [codigo=" + codigo + ", valor=" + valor + ", cpf=" + cpf + ", voos=" + voos + "]";
+    }
+    
+}
